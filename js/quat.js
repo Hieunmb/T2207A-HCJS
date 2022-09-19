5cf01d00d7474ff9d1ba2c288602bd75bb802198
Hieunmb <zhuongdae@gmail.com>
2022-09-19 13:58:49 +0700

    bai tap

--- js/quat.js
+++ js/quat.js
@@ -2,18 +2,18 @@
     function quay() {
         var x = document.getElementById("quat");
         x.style.transform='rotate('+f+'deg)';
-        f+=1;
+        f+=5;
     }
     var at
     function quayTron1(){
         clearInterval(at);
-        at=setInterval(quay,15);
+        at=setInterval(quay,20);
     }function quayTron2(){
         clearInterval(at);
         at=setInterval(quay,10);
     }function quayTron3(){
         clearInterval(at);
-        at=setInterval(quay,5);
+        at=setInterval(quay,1);
     }
     function quatdung(){
         clearInterval(at);
